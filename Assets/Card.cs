c5747e94e6b23249e5e05ba8071e79f6a5e7c6ef
yuuki1827 <kouaka.hibatsuna@gmail.com>
2020-02-09 01:10:31 +0900

    カード♠︎♣︎♡♢のA~Kの13枚+joker合わせて53枚をシャッフルして、 15列*4行でコンソールに表示を行う

---
+++ Assets/Card.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// カードのデータを保持するクラス
+/// 数字・マーク・ジョーカー判定の3項目を持つ
+/// </summary>
+public struct Card
+{
+    /* カードのマーク */
+    public int suit;
+
+    /* カードの数字 */
+    public int number;
+
+    /* ジョーカー判定 */
+    // Jokerの場合、マーク・数字に関係なくJoker扱いとする
+    public bool isJoker;
+
+    public Card(int suit, int number, bool isJoker)
+    {
+        this.suit = suit;
+        this.number = number;
+        this.isJoker = isJoker;
+    }
+}
