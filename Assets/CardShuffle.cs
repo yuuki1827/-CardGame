627e6183c1f55ae32d733e6a7745988d054ce202
yuuki1827 <kouaka.hibatsuna@gmail.com>
2020-02-11 06:52:32 +0900

    カードの生成をcardクラスで行うように修正

--- Assets/CardShuffle.cs
+++ Assets/CardShuffle.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// カードをシャッフルするクラス
@@ -10,21 +11,17 @@ public class CardShuffle : MonoBehaviour
     /* カードを保持するリスト */
     List<Card> cardList;
 
-    /* 各マークでの枚数を判定 */
-    const int numbersInSuit = 13;
-
-    /* 各マークのIDを決める */
-    const int Spade = 0;
-    const int Club = 1;
-    const int Heart = 2;
-    const int Diamond = 3;
-    List<int> suits = new List<int>() { Spade, Club, Heart, Diamond };
+    List<int> suits = new List<int>()
+    {
+        Card.Spade, Card.Club,
+        Card.Heart, Card.Diamond
+    };
 
     void Start()
     {
         InitializeCordList();
         ShuffleCards();
-        ShowCardName();
+        //ShowCardName();
     }
 
     /// <summary>
@@ -38,7 +35,7 @@ public class CardShuffle : MonoBehaviour
         // 各マークごとにカードを生成
         foreach(int suit in suits)
         {
-            for (int i = 1; i <= numbersInSuit; i++)
+            for (int i = 1; i <= Card.numbersInSuit; i++)
             {
                 Card card = new Card(suit, i, false);
                 cardList.Add(card);
@@ -50,143 +47,85 @@ public class CardShuffle : MonoBehaviour
         cardList.Add(joker);
     }
 
-    /// <summary>
-    /// コンソールに出力する
-    /// </summary>
-    void ShowCardName()
+    void InitializeCade(List<Card> list, GameObject cardObject, string spriteName)
     {
-
-        //
-
-        //// コンソールに表示する
-        //Debug.Log(cardText);
-
-        System.Text.StringBuilder sb = new System.Text.StringBuilder();
-        string separator = ", ";
-        foreach (Card card in cardList)
-        {
-            if (card.isJoker)
-            {
-                sb.Append("JOKER").Append(separator);
-            }
-            else
-            {
-                sb.Append(GetSuitMark(card.suit)).
-                    Append(GetSpecialNumber(card.number)).Append(separator);
-            }
-        }
-        string cardText = sb.ToString();
-
-        // 一行あたり15枚のカードを表示する
-        int cardInLine = 15;
-        int cardCount = 0;
-
-        // 区切り文字を使用して、カードを一枚ごとの配列に格納する
-        string[] separatorChar = { separator };
-        string[] cards = cardText.Split(separatorChar,
-            System.StringSplitOptions.RemoveEmptyEntries);
-        sb = new System.Text.StringBuilder();
-        for(int i = 0; i < cards.Length; i++)
-        {
-            sb.Append(cards[i]).Append(separator);
-            cardCount++;
-            // 表示枚数に達していたらコンソールに表示
-            if(cardCount < cardInLine)
-            {
-                continue;
-            }
-            string line = sb.ToString();
-            if(i == cards.Length - 1)
-            {
-                line = RemoveLastSeparator(line, separator);
-            }
-            Debug.Log(line);
-            sb = new System.Text.StringBuilder();
-            cardCount = 0;
-        }
-        // 最後のカンマだけ削除する
-        string lastLine = sb.ToString();
-        if(lastLine.Length > 0)
-        {
-            lastLine = RemoveLastSeparator(lastLine, separator);
-            Debug.Log(lastLine);
-        }
+        // 画像アトラスを取得
+        SpriteAtlas atlas = Resources.Load<SpriteAtlas>("Sprites/Atlas/" + spriteName);
     }
 
-    /// <summary>
-    /// 置換処理（カンマの削除）
-    /// </summary>
-    /// <param name="line">文字列</param>
-    /// <param name="separator">カンマ</param>
-    /// <returns>削除後の文字列</returns>
-    string RemoveLastSeparator(string line, string separator)
-    {
-        int index = line.LastIndexOf(separator);
-        if(index != -1)
-        {
-            line = line.Remove(index);
-        }
-        return line;
-    }
 
-    /// <summary>
-    /// IDに応じたマークを返す
-    /// </summary>
-    /// <param name="suit">マークID</param>
-    /// <returns></returns>
-    string GetSuitMark(int suit)
-    {
-        string suitMark;
-        switch (suit)
-        {
-            case Spade:
-                suitMark = "♠︎";
-                break;
-            case Club:
-                suitMark = "♣︎";
-                break;
-            case Heart:
-                suitMark = "♡";
-                break;
-            case Diamond:
-                suitMark = "♢";
-                break;
-            default:
-                suitMark = "JOKER";
-                break;
-        }
-        return suitMark;
-    }
+    ///// <summary>
+    ///// コンソールに出力する
+    ///// </summary>
+    //void ShowCardName()
+    //{
+    //    System.Text.StringBuilder sb = new System.Text.StringBuilder();
+    //    string separator = ", ";
+    //    foreach (Card card in cardList)
+    //    {
+    //        if (card.isJoker)
+    //        {
+    //            sb.Append("JOKER").Append(separator);
+    //        }
+    //        else
+    //        {
+    //            sb.Append(Card.GetSuitMark(card.suit)).
+    //                Append(Card.GetSpecialNumber(card.number)).Append(separator);
+    //        }
+    //    }
+    //    string cardText = sb.ToString();
 
-    /// <summary>
-    /// 1,11~13をアルファベットにして返す
-    /// </summary>
-    /// <param name="number">数字</param>
-    /// <returns></returns>
-    string GetSpecialNumber(int number)
-    {
-        if(1 < number && number < 11)
-        {
-            return number.ToString();
-        }
-        string special = "None";
-        switch (number)
-        {
-            case 1:
-                special = "A";
-                break;
-            case 11:
-                special = "J";
-                break;
-            case 12:
-                special = "Q";
-                break;
-            case 13:
-                special = "K";
-                break;
-        }
-        return special;
-    }
+    //    // 一行あたり15枚のカードを表示する
+    //    int cardInLine = 15;
+    //    int cardCount = 0;
+
+    //    // 区切り文字を使用して、カードを一枚ごとの配列に格納する
+    //    string[] separatorChar = { separator };
+    //    string[] cards = cardText.Split(separatorChar,
+    //        System.StringSplitOptions.RemoveEmptyEntries);
+    //    sb = new System.Text.StringBuilder();
+    //    for(int i = 0; i < cards.Length; i++)
+    //    {
+    //        sb.Append(cards[i]).Append(separator);
+    //        cardCount++;
+    //        // 表示枚数に達していたらコンソールに表示
+    //        if(cardCount < cardInLine)
+    //        {
+    //            continue;
+    //        }
+    //        string line = sb.ToString();
+    //        if(i == cards.Length - 1)
+    //        {
+    //            line = RemoveLastSeparator(line, separator);
+    //        }
+    //        Debug.Log(line);
+    //        sb = new System.Text.StringBuilder();
+    //        cardCount = 0;
+    //    }
+    //    // 最後のカンマだけ削除する
+    //    string lastLine = sb.ToString();
+    //    if(lastLine.Length > 0)
+    //    {
+    //        lastLine = RemoveLastSeparator(lastLine, separator);
+    //        Debug.Log(lastLine);
+    //    }
+    //}
+
+    ///// <summary>
+    ///// 置換処理（カンマの削除）
+    ///// </summary>
+    ///// <param name="line">文字列</param>
+    ///// <param name="separator">カンマ</param>
+    ///// <returns>削除後の文字列</returns>
+    //string RemoveLastSeparator(string line, string separator)
+    //{
+    //    int index = line.LastIndexOf(separator);
+    //    if(index != -1)
+    //    {
+    //        line = line.Remove(index);
+    //    }
+    //    return line;
+    //}
 
     /// <summary>
     /// カードをシャッフルする
